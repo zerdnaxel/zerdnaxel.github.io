599dfdd558dcf97bad3e9fd6c6a0360951c1bc85
Alexandre D'Ambra <alexandre.d-ambra@univ-amu.fr>
2026-06-10 14:48:47 +0200

    Added Basic CV JSON, test Vue components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 import Pending from './components/Pending.vue'
-
 </script>
 
 <template>
-  <Pending/>
+  <Pending />
+
 </template>
--- src/components/Pending.vue
+++ src/components/Pending.vue
@@ -1,8 +1,25 @@
-<script setup>
-import { ref } from 'vue'
+<script>
+import Hero from './Hero.vue';
+import ThemeCtlr from './ThemeCtlr.vue';
+export default {
+    data() {
+        return {
+            btnContent: 'Button'
+        }
+    },
+    methods: {
+        ChangeButton() {
+            this.btnContent = "(not a) Button";
+        }
+    }
+}
 </script>
+
 <template>
+    <Hero />
     <h1>Welcome</h1>
     <h2>we are not here yet</h2>
     <p>this website is not finished yet, please wait!!!</p>
+    <button class="btn btn-primary" @click="ChangeButton">{{ btnContent }}</button>
+
 </template>
