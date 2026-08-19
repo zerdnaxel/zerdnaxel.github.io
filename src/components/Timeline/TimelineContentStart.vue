fa1d99cc5e6a94d43f00594a6c9b8ece6346349d
Alexandre D'Ambra <alexandre.d-ambra@univ-amu.fr>
2026-08-19 22:50:32 +0200

    see previous

--- src/components/Timeline/TimelineContentStart.vue
+++ src/components/Timeline/TimelineContentStart.vue
@@ -18,13 +18,13 @@ const props = defineProps({
             </svg>
         </div>
         <div v-if="index % 2 == 1" class="timeline-end md:mb-10">
-            <time class="font-mono italic">{{ when }} ({{ timeLenght }})</time>
+            <time class="font-mono italic">{{ when }} ({{ timelength }})</time>
             <div class="text-lg font-black">{{ what }}</div>
             {{ where }}
             <div v-if="about != null">{{ about }}</div>
         </div>
         <div v-else class="timeline-start mb-10 md:text-end">
-            <time class="font-mono italic">{{ when }} ({{ timeLenght }})</time>
+            <time class="font-mono italic">{{ when }} ({{ timelength }})</time>
             <div class="text-lg font-black">{{ what }}</div>
             {{ where }}
             <div v-if="about != null">{{ about }}</div>
@@ -40,14 +40,16 @@ const props = defineProps({
             </svg>
         </div>
         <div v-if="index % 2 == 1" class="timeline-end md:mb-10">
-            <time class="font-mono italic">{{ when }}</time>
+            <time class="font-mono italic">{{ when }} ({{ timelength }})</time>
             <div class="text-lg font-black">{{ what }}</div>
             {{ where }}
+            <div v-if="about != null">{{ about }}</div>
         </div>
         <div v-else class="timeline-start mb-10 md:text-end">
-            <time class="font-mono italic">{{ when }}</time>
+            <time class="font-mono italic">{{ when }} ({{ timelength }})</time>
             <div class="text-lg font-black">{{ what }}</div>
             {{ where }}
+            <div v-if="about != null">{{ about }}</div>
         </div>
     </li>
 </template>
